db44c44cb3fcb40226813abf8257c6076b4e73d0
randomnoeimyapolzovatelya <randomnoeimyapolzovatelya@gmail.com>
2025-12-19 21:13:44 +0300

    2

--- pnlk_Telegram_Chat_First_bot.py
+++ pnlk_Telegram_Chat_First_bot.py
@@ -1,1 +1,28 @@
-# 8305056913:AAEIBgy5cPURt9Qbg-hmtwpGqNxtxgchNqQ
+
+
+# python.exe -m pip install --upgrade pip
+# pip install telegram 
+# pip install python-telegram-bot
+
+
+import os
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+
+TOKEN = os.environ.get('BOT_TOKEN') # Токен возьмем из переменной среды
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('Привет! Я бот.')
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(update.message.text)
+
+def main():
+    app = Application.builder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    print("Бот запущен...")
+    app.run_polling()
+
+if __name__ == '__main__':
+    main()
